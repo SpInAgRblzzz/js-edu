85be53aad64a283aaf73bebb349f80212453ef22
SpInAgRblzzz <36811343+SpInAgRblzzz@users.noreply.github.com>
2019-09-29 20:32:41 +0300

    task solved

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ module.exports = function getTimeForEducation(
     knowsProgramming = true,
     config = {family: 4}
     ) {
-      return 0;
+      const timeRquired = knowsProgramming ? 800 : 1300;
+
+      return Math.ceil(timeRquired/config[focus]);
   };
   
